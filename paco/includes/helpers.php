3bbd7029daa22ef6cc3b8cabefaf2c72fc3d58e2
Herperino <l.nasc@live.com>
2017-05-12 00:03:43 -0300

    Functions be here
    PHP functions that were spread all around will be centralized here for now

--- paco/includes/helpers.php
+++ paco/includes/helpers.php
@@ -67,5 +67,75 @@
         exit;
     }
     
+    /**
+     * Takes data from the server to display the prescription list 
+     * for a registered patient.
+     * 
+     * It must be called within a table div.
+     */
+    function displayPrescription($prescriptions){
+        
+        print("<th>Paciente</th>");
+        print("<th>Data</th>");
+        print("<th colspan='13'>Medicamentos </th>");
+            if (!empty($prescriptions)){
+            foreach($prescriptions as $prescription){
+                 $prescription = $sub = array_slice($prescription, 2, null, true); //Remove ID and userID from array
+                    
+                print("<tr>"); 
+                print("<td>" . $prescription["patientID"]. "</td>");
+                print("<td>" . $prescription["Date"]. "</td>");
+                for($i = 1; $i <= 10; $i++){
+                    print("<td>". $prescription["med".$i] ." ". $prescription["pos".$i]."</td>");
+                }
+             print("<td>"); 
+             print("<input  data-patient =" . (string)$prescription['patientID'] ."
+                    data-operation = \"PRESCRIPTION_EDIT\"
+                     data-timestamp ='" . (string)$prescription['Date'] . 
+                    "' type = 'button' onClick = 'prescriptionHandler(this)' 
+                    class= 'btn btn-success' value='Editar Prescrição'/>"); print("</td></tr>");}}
+    }
     
+    /**
+     * Takes data from the server to display the result list 
+     * for a registered patient.
+     *  
+     * It must be called within a table div.
+     */
+    function displayResults($labresults){
+        print("<th>Paciente</th>" . "<th>Data</th>" . "<th colspan= '13'> Exames</th>");
+        
+        if(!empty($labresults)){
+            foreach($labresults as $result){
+                
+              $labref = $sub = array_slice($result, 1, null, true); //Removes index and userID from the array
+              
+              print("<tr>");
+              
+              foreach ($labref as $key=>$item){
+                  
+                 $currentkey = $key;
+                  
+                 if(strcmp($currentkey,"Date") == 0 || strcmp($currentkey,"patientID") == 0){
+                      print("<td>". $item . "</td>");
+                  }
+                  else{
+                   
+                    if($item !=  null)
+                        print("<td>". $key. "<br> " .$item. "</td>");
+                     else
+                        print("<td>". $key. "<br> --</td>");
+                  }
+              }
+              
+              print("<td>");
+              print("<input  data-patient = ". (string)$result['patientID'] .
+                    "data-operation = 'LAB_EDIT'
+                     data-timestamp ='" . (string)$result['Date'] . 
+                    "' type = 'button' onClick = 'labHandler(this, 'labref.php')' 
+                    class= 'btn btn-success' value='Editar resultados'/>");
+              print("</td>"."</tr>");
+            }
+        }
+    }
 ?>
