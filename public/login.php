8f2d5ec51021e4e550cfd8848e5477c89e418037
Herperino <l.nasc@live.com>
2017-05-13 19:25:22 -0300

    small fixes

--- public/login.php
+++ public/login.php
@@ -9,6 +9,22 @@
         if($_SERVER["REQUEST_METHOD"] == "POST")
         {
             //init postgres connection
+            $database = "PACO_users"
+            try{
+            $conn = pg_connect("host=ec2-23-21-227-73.compute-1.amazonaws.com port=5432 dbname=".$database."user=hypmpmdpmsubvi password=d4338194bb3376272ff09a413786ed3852229812b977259d5d4b5e7958c37c85");// enable sessions
+
+              // query database for user
+              try{
+              $users = pg_query($conn, "SELECT * FROM PACO_users WHERE username = ?", $_POST["id"]);
+              }
+              catch(Exception $e){
+                render("apology.php", ['errormessage' => htmlspecialchars("test")]);
+              }
+            }
+            catch(Exception $e){
+              render("apology.php", ['errormessage' => htmlspecialchars("test")]);
+            }
+
 
             // if we found user, check password
             if (count($users) == 1)
