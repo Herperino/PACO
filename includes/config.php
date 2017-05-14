d103b663527cfdc23b81d4e389aaea72ac5dc8e9
Herperino <l.nasc@live.com>
2017-05-13 23:37:29 -0300

    changed config and register

--- includes/config.php
+++ includes/config.php
@@ -26,6 +26,10 @@
     $database = "da9ca7l565c2pg";
     try{
     $conn = pg_connect("host=ec2-23-21-227-73.compute-1.amazonaws.com port=5432 dbname=".$database." user=hypmpmdpmsubvi password=d4338194bb3376272ff09a413786ed3852229812b977259d5d4b5e7958c37c85 sslmode=require");
+    }
+    catch(Exception $e){
+      error_log($e);
+    }
 
     //require authentication for all pages except /login.php, /logout.php, and /register.php
     if (!in_array($_SERVER["PHP_SELF"], ["/login.php", "/logout.php", "/register.php"]))
