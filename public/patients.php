08e0a52969f48d6ce139b79a2e40acb7c79f9af4
Herperino <l.nasc@live.com>
2017-05-17 00:58:42 -0300

    connect pg

--- public/patients.php
+++ public/patients.php
@@ -66,6 +66,9 @@
 
         //Get patient and it's status from the database
         require_once("../includes/helpers.php");
+
+        $conn = db_connect();
+
         $query = "SELECT * FROM public.\"patients\" WHERE patientid = '" . $patientID ."'";
         $res = pg_query($conn, $query);
 
