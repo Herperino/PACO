650fc2a716425f38a4938f292b4bd54ed11813ef
Herperino <l.nasc@live.com>
2017-05-16 23:35:19 -0300

    psql changes

--- includes/config.php
+++ includes/config.php
@@ -23,13 +23,7 @@
     session_start();
 
     //init postgres connection
-    $database = "da9ca7l565c2pg";
-    try{
-    $conn = pg_connect("host=ec2-23-21-227-73.compute-1.amazonaws.com port=5432 dbname=".$database." user=hypmpmdpmsubvi password=d4338194bb3376272ff09a413786ed3852229812b977259d5d4b5e7958c37c85 sslmode=require");
-    }
-    catch(Exception $e){
-      error_log($e);
-    }
+    $conn = connect_db();
 
     //require authentication for all pages except /login.php, /logout.php, and /register.php
     if (!in_array($_SERVER["PHP_SELF"], ["/login.php", "/logout.php", "/register.php"]))
--- includes/helpers.php
+++ includes/helpers.php
@@ -138,4 +138,22 @@
             }
         }
     }
+
+    function connect_db(){
+
+      //database name
+      $database = "da9ca7l565c2pg";
+
+      try{
+      $conn = pg_connect("host=ec2-23-21-227-73.compute-1.amazonaws.com port=5432
+                          dbname=".$database."
+                          user=hypmpmdpmsubvi
+                          password=d4338194bb3376272ff09a413786ed3852229812b977259d5d4b5e7958c37c85
+                          sslmode=require");
+      }
+      catch(Exception $e){
+        error_log($e);
+      }
+
+    }
 ?>
