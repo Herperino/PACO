106c44736abab58cf068fc7895abb90902059f46
Herperino <l.nasc@live.com>
2017-05-17 01:30:05 -0300

    fuck

--- public/patients.php
+++ public/patients.php
@@ -72,7 +72,7 @@
         $status = $patient[6]; //6 = p_status
 
         for($i = 0; $i<sizeof($patient); $i++){
-          print($patient[$i]);
+          print("".$patient[$i]);
         }
         //Change status given current status state
         if($status == 1) { pg_query($conn,"UPDATE public.\"patients\" SET p_status = 0 WHERE patientid = '".$patientID."'");}
