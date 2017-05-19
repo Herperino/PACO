ce341ebdc19097fabaa9bf5481bd2fe0c3366b3b
Herperino <l.nasc@live.com>
2017-05-18 23:50:46 -0300

    trying to fix form

--- views/pagebottom.php
+++ views/pagebottom.php
@@ -1,6 +1,6 @@
 
     </div>
-        <div class= "pagebottom navbar-fixed-bottom"> 
+        <div class= "pagebottom navbar-default"> 
             <center><p> <table>PACO | Programa de acompanhamento - 2016 - 2017</table> </p></center>
         </div>
     </BODY>
