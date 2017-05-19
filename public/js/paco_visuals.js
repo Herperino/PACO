57299d534407e4421221151e1b736cf054ca0ce9
Herperino <l.nasc@live.com>
2017-05-18 23:18:13 -0300

    trying to fix form

--- public/js/paco_visuals.js
+++ public/js/paco_visuals.js
@@ -156,9 +156,9 @@ function renderPrescriptionForm(parameters){
 
     for(var i = 1; i <= 10; i++){
 
-        content+= i + ". Medicamento<input name= 'med"+ i + "' value='"+ parameters.last_p['med'+i] +"' type = 'text' placeholder = 'Inserir'></input><br>";
-        content+= "Dose<input name= 'dos"+ i + "' type = 'text' placeholder = 'Inserir'></input><br>";
-        content+= "Via<input name= 'via"+ i + "' type = 'text'  placeholder = 'Inserir'></input><br>";
+        content+= i + ". Medicamento<input name= 'med"+ i + "' value='"+ parameters.last_p['med'+i] +"' type = 'text' placeholder = 'Inserir'></input>";
+        content+= "Dose<input name= 'dos"+ i + "' type = 'text' placeholder = 'Inserir'></input>";
+        content+= "Via<input name= 'via"+ i + "' type = 'text'  placeholder = 'Inserir'></input>";
         content+= "Posologia <select class = 'custom-select mb-2 mr-sm-2 mb-sm-0' name= 'pos"+ i +"'>";
         content+= "<option value = 'null'> Selecione </option>";
         content+= "<option value = '1x/d'> 1x/dia </option>";
