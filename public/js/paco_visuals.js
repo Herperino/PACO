bb797e803389d79bb25b19bae91a2342cca1d496
Herperino <l.nasc@live.com>
2017-05-18 22:51:06 -0300

    trying to fix form

--- public/js/paco_visuals.js
+++ public/js/paco_visuals.js
@@ -132,7 +132,7 @@ function renderPrescriptionForm(parameters){
 
     //For prescription handling, the controller can only be acompanhamento.php
     var controller = "acompanhamento.php";
-    var parameters.last_p;
+    var parameters.last_p = {};
 
     if(parameters['operation'] == "PRESCRIPTION_ADD") {
         var info = 'Adicionar nova prescrição';
