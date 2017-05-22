6e20711949ee796b900aeed01282d36fa23a4c2f
Herperino <l.nasc@live.com>
2017-05-22 02:37:18 -0300

    adding delete functionality

--- public/acompanhamento.php
+++ public/acompanhamento.php
@@ -58,7 +58,7 @@
         }
         else if($_POST['operation'] == "DELETE_PRESCRIPTION"){
           //Deletes selected prescription
-          print("HEYHEYHEYHEYEHEY")
+          print("HEYHEYHEYHEYEHEY");
           $query = pg_query($conn, "DELETE FROM public.\"prescriptions\" WHERE date ='".$_POST['date']."'");
         }
 
