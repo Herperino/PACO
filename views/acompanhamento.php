2d30147a23bd4c2ce1b6b44ff441f0b575ce1048
Herperino <l.nasc@live.com>
2017-05-22 00:46:11 -0300

    fixing forms 22/5 SQL Time

--- views/acompanhamento.php
+++ views/acompanhamento.php
@@ -24,7 +24,7 @@
     <div class= "row" style = "overflow-x:auto">
         <div class="panel panel-default">
             <div class="panel-heading">
-                <h4><?php echo $patientID ?></h4>
+                <h4><?php echo $patientID ?> - <?php echo $P_ID ?> </h4>
             </div>
 
             <div id ="prescription_list" class="panel-body">
