71133d05139cdca0654b4f11b32097585cb20f61
Herperino <l.nasc@live.com>
2017-05-24 00:27:39 -0300

    Implementando labresults

--- public/patients.php
+++ public/patients.php
@@ -54,7 +54,9 @@
         }
         else if(strcmp($operation,"DELETE_PTT") == 0){
             pg_query($conn, "DELETE FROM public.\"patients\" WHERE patientid ='".$patientID."'
-            AND userid ='".$_SESSION['id']."'");
+            AND userid =".$_SESSION['id'].);
+
+
         }
 
         //Returns to the original page
