d774ebed05ba1c175b00cb3f6a73a37edf27ce58
Herperino <l.nasc@live.com>
2017-05-24 00:24:28 -0300

    Implementando labresults

--- public/patients.php
+++ public/patients.php
@@ -52,7 +52,7 @@
 
             addPatient($conn);
         }
-        else if(strcmp($operation,"DELETE_PTT")){
+        else if(strcmp($operation,"DELETE_PTT") == 0){
             pg_query($conn, "DELETE FROM public.\"patients\" WHERE patientid ='".$patientID."'
             AND userid ='".$_SESSION['id']."'");
         }
