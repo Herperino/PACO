080d80f8618358e73d30bb5ae5ecd39f9c8da305
Herperino <l.nasc@live.com>
2017-05-27 23:51:15 -0300

    Fixing labref

--- views/labref.php
+++ views/labref.php
@@ -11,7 +11,7 @@
 
 <div class ="container">
     <br><br>
-    <div class= "row">
+    <div class= "row" style="overflow-x:auto">
         <div class="panel panel-default">
         <div class="panel-heading"><h4><?php echo $patientID ?></h4></div>
 
