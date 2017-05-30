b3b152f2644763bfc873f7c558c8a73b259add3d
Herperino <l.nasc@live.com>
2017-05-29 22:38:12 -0300

    adapting tables

--- views/acompanhamento.php
+++ views/acompanhamento.php
@@ -48,7 +48,7 @@
 
 <?php else: ?>
 <div class = 'container'>
-    <div class = 'page-header'><h2>Acompanhamento de prescrições</h2></div>
+    <div class = 'page-header' style="overflow-x:auto"><h2>Acompanhamento de prescrições</h2></div>
     <h4>Pacientes em acompanhamento por <?php echo $_SESSION['username'] ?></h4>
     <div id="patient_list" class = "panel panel-default">
         <script>
