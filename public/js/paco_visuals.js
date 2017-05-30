e284812d17d09b9096cf938a2750dac45c4c3c0f
Herperino <l.nasc@live.com>
2017-05-29 23:39:44 -0300

    Improving readability of paco_visuals

--- public/js/paco_visuals.js
+++ public/js/paco_visuals.js
@@ -331,3 +331,18 @@ function submitModal(){
   jQuery(document).ready(function($) {
     $('.fake-select').fakeSelect();
   });
+
+  var headertext = [],
+  headers = document.querySelectorAll("#patients th"),
+  tablerows = document.querySelectorAll("#patients th"),
+  tablebody = document.querySelector("#patients tbody");
+
+for(var i = 0; i < headers.length; i++) {
+  var current = headers[i];
+  headertext.push(current.textContent.replace(/\r?\n|\r/,""));
+} 
+for (var i = 0, row; row = tablebody.rows[i]; i++) {
+  for (var j = 0, col; col = row.cells[j]; j++) {
+    col.setAttribute("data-th", headertext[j]);
+  } 
+}
