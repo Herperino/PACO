03a88059865923ecc181ddba6f35d76abff053f3
Herperino <l.nasc@live.com>
2017-06-07 23:12:24 -0300

    Alterando o apology.php para mostrar uma mensagem mais bonitinha

--- views/apology.php
+++ views/apology.php
@@ -1,6 +1,6 @@
-<div class = "jumbotron">
-	<div class='alert alert-warning'>
+<br>
+<br>
+<div class = "jumbotron" style="background-color: lightyellow">
 	<h2>Oops, desculpas!</h2>
 	<p><?php echo($errormessage)?></p>
-	</div>
 </div>
