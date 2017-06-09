55fa341d51cc1d2267d50b65abaf482f9b88639d
Herperino <l.nasc@live.com>
2017-06-09 17:33:22 -0300

    Fontes do google

--- views/landing.php
+++ views/landing.php
@@ -31,26 +31,26 @@
                     <div class = 'container'>
                         <div class ='row'>
                             <div class = 'col-xs-12 col-sm-12 col-md-12 col-lg-12'>    
-                                
-                                <p class= 'content'> O Acompanhamento Farmacoterapêutico é definido como uma prática personalizada na qual o farmacêutico tem a responsabilidade de orientar o paciente, além de detectar, prevenir e resolver todos os problemas relacionados com medicamentos (PRM) de uma maneira contínua, sistemática e documentada, em colaboração com o paciente e equipe multiprofissional. O <strong>programa de acompanhamento farmacêutico</strong> é uma ferramenta elaborada por mim (Leon Nascimento) para facilitar o acompanhamento de prescrições de pacientes internados em ambiente hospitalar ou ambulatorial.
-                                </p>    
-
+                                                 
                                 <div class = 'row'>
-                                <div class = 'icons col-xs-12 col-sm-4 col-md-4 col-lg-4'>
-                                        <h2>ACOMPANHE</h2>
-                                        <img class = 'ico' src='https://image.flaticon.com/icons/svg/1/1755.svg' alt='View details free icon' title='View details free icon'>
+                                    <div class = 'icons col-xs-12 col-sm-4 col-md-4 col-lg-4'>
+                                            <h2>Acompanhe</h2>
+                                            <img class = 'ico' src='https://image.flaticon.com/icons/svg/1/1755.svg' alt='View details free icon' title='View details free icon'>
 
-                                    </div>
-                                <div class = 'icons col-xs-12 col-sm-4 col-md-4 col-lg-4'>
-                                        <h2>AVALIE</h2>
-                                        <img class ='ico' src='https://image.flaticon.com/icons/svg/344/344074.svg' alt='Evaluation free icon' title='Evaluation free icon'>
-                                    </div>
+                                        </div>
+                                    <div class = 'icons col-xs-12 col-sm-4 col-md-4 col-lg-4'>
+                                            <h2>Avalie</h2>
+                                            <img class ='ico' src='https://image.flaticon.com/icons/svg/344/344074.svg' alt='Evaluation free icon' title='Evaluation free icon'>
+                                        </div>
 
-                                <div class = 'icons col-xs-12 col-sm-4 col-md-4 col-lg-4'>
-                                            <h2>DISCUTA<h2>
-                                            <img class ='ico' src='https://image.flaticon.com/icons/svg/134/134807.svg' alt='Chat free icon' title='Chat free icon'>
-                                    </div>
-                                </div>             
+                                    <div class = 'icons col-xs-12 col-sm-4 col-md-4 col-lg-4'>
+                                                <h2>Discuta<h2>
+                                                <img class ='ico' src='https://image.flaticon.com/icons/svg/134/134807.svg' alt='Chat free icon' title='Chat free icon'>
+                                        </div>
+                                </div>
+
+                                <p class= 'content'> O Acompanhamento Farmacoterapêutico é definido como uma prática personalizada na qual o farmacêutico tem a responsabilidade de orientar o paciente, além de detectar, prevenir e resolver todos os problemas relacionados com medicamentos (PRM) de uma maneira contínua, sistemática e documentada, em colaboração com o paciente e equipe multiprofissional. O <strong>programa de acompanhamento farmacêutico</strong> é uma ferramenta elaborada por mim (Leon Nascimento) para facilitar o acompanhamento de prescrições de pacientes internados em ambiente hospitalar ou ambulatorial.
+                                </p>                 
                                         
                             </div>
                         </div>
