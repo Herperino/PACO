1f0e48eebaf6ddf0657d7c14cf98b3611d80f54c
Herperino <l.nasc@live.com>
2017-06-09 17:40:26 -0300

    Embelezando landing

--- views/landing.php
+++ views/landing.php
@@ -36,21 +36,27 @@
                                     <div class = 'icons col-xs-12 col-sm-4 col-md-4 col-lg-4'>
                                             <h2>Acompanhe</h2>
                                             <img class = 'ico' src='https://image.flaticon.com/icons/svg/1/1755.svg' alt='View details free icon' title='View details free icon'>
-
+                                            <br><br>
+                                            <small>Adicione prescrições e resultados laboratoriais de forma fácil</small>
                                         </div>
                                     <div class = 'icons col-xs-12 col-sm-4 col-md-4 col-lg-4'>
                                             <h2>Avalie</h2>
                                             <img class ='ico' src='https://image.flaticon.com/icons/svg/344/344074.svg' alt='Evaluation free icon' title='Evaluation free icon'>
+                                            <br><br>
+                                            <small>Visualize prescrições anteriores e contraste com resultados laboratoriais</small>
                                         </div>
 
                                     <div class = 'icons col-xs-12 col-sm-4 col-md-4 col-lg-4'>
                                                 <h2>Discuta<h2>
                                                 <img class ='ico' src='https://image.flaticon.com/icons/svg/134/134807.svg' alt='Chat free icon' title='Chat free icon'>
+                                            <br><br>
+                                            <small>Comente e discuta sobre os seus pacientes de maneira rápida e simples</small>
                                         </div>
                                 </div>
 
-                                <p class= 'content'> O Acompanhamento Farmacoterapêutico é definido como uma prática personalizada na qual o farmacêutico tem a responsabilidade de orientar o paciente, além de detectar, prevenir e resolver todos os problemas relacionados com medicamentos (PRM) de uma maneira contínua, sistemática e documentada, em colaboração com o paciente e equipe multiprofissional. O <strong>programa de acompanhamento farmacêutico</strong> é uma ferramenta elaborada por mim (Leon Nascimento) para facilitar o acompanhamento de prescrições de pacientes internados em ambiente hospitalar ou ambulatorial.
-                                </p>                 
+                                <br><br>
+                                <div class= 'content'> O Acompanhamento Farmacoterapêutico é definido como uma prática personalizada na qual o farmacêutico tem a responsabilidade de orientar o paciente, além de detectar, prevenir e resolver todos os problemas relacionados com medicamentos (PRM) de uma maneira contínua, sistemática e documentada, em colaboração com o paciente e equipe multiprofissional. O <strong>programa de acompanhamento farmacêutico</strong> é uma ferramenta elaborada por mim (Leon Nascimento) para facilitar o acompanhamento de prescrições de pacientes internados em ambiente hospitalar ou ambulatorial.
+                                </div>                 
                                         
                             </div>
                         </div>
