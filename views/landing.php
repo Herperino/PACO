0fc892629377ae43f3c63902c055a5c008af8d68
Herperino <l.nasc@live.com>
2017-06-08 23:50:15 -0300

    Testando se php é que nem js e tem falsy e truthy

--- views/landing.php
+++ views/landing.php
@@ -9,14 +9,17 @@
         print("<div class = 'jumbotron bg1'>
                     <div class='container'>
                         <br>
-                            <div class= 'title' style='font-family:Helvetica' >
+                            <div class= 'title'>
                                 <h1> PACO </h1>
-                                <h4> Programa de acompanhamento farmacoterapêutico</h4>
-                                
+                                <h3> Programa de acompanhamento farmacoterapêutico</h3>
+                                <br>
                                 Todos os seus pacientes em um só lugar.
                                 <br>
-                                <small>Acompanhe, avalie, discuta.<small>
+                                
+                                <small>Acompanhe, avalie, discuta.</small>
 
+                                <br>
+                                <br>
                                 <br>
                                 <div>
                                     <a href='register.php'><button type='button' class='btn btn-success'> Inscreva-se </button></a>
@@ -25,20 +28,21 @@
                     </div>    
                 </div>
                 <div class = 'jumbotron' id = 'Sobre'>
-                <div class = 'container'>
-                    <div class ='row'>
-                                    <div class = 'content col-xs-12 col-sm-12 col-md-12 col-lg-12'>
-                                        O Acompanhamento Farmacoterapêutico é definido como uma prática personalizada na qual o 
-                                        farmacêutico tem a responsabilidade de orientar o paciente, 
-                                        além de detectar, prevenir e resolver todos os problemas relacionados com medicamentos (PRM) de uma maneira
-                                        contínua, sistemática e documentada, em colaboração com o paciente e equipe multiprofissional. 
-                                        O <strong>programa de acompanhamento farmacêutico</strong> é uma ferramenta elaborada por mim (Leon Nascimento)
-                                        para realizar o acompanhamento de prescrições de pacientes internados em ambiente hospitalar ou ambulatorial
-                                    
-                                        <br>
+                    <div class = 'container'>
+                        <div class ='row'>
+                            <div class = 'content col-xs-12 col-sm-12 col-md-12 col-lg-12'>    
+                                
+                                <p> O Acompanhamento Farmacoterapêutico é definido como uma prática personalizada na qual o farmacêutico tem a responsabilidade de orientar o paciente, além de detectar, prevenir e resolver todos os problemas relacionados com medicamentos (PRM) de uma maneira contínua, sistemática e documentada, em colaboração com o paciente e equipe multiprofissional. O <strong>programa de acompanhamento farmacêutico</strong> é uma ferramenta elaborada por mim (Leon Nascimento) para facilitar o acompanhamento de prescrições de pacientes internados em ambiente hospitalar ou ambulatorial.
+                                </p>    
+
+                                <div class = 'row'>
+                                <div class = 'content col-xs-4 col-sm-4 col-md-4 col-lg-4'>MOCHILA</div>
+                                <div class = 'content col-xs-4 col-sm-4 col-md-4 col-lg-4'>MOCHILA</div>
+                                <div class = 'content col-xs-4 col-sm-4 col-md-4 col-lg-4'>MOCHILA</div>
+                                </div>             
                                         
-                                    </div>
-                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>"
         );
