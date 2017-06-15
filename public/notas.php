77d4a54250b54b2d1a05f3229982cd3c2232b9e2
Herperino <l.nasc@live.com>
2017-06-14 21:39:48 -0300

    Segregando a parte de usuários com a parte de manutenção

--- public/notas.php
+++ public/notas.php
@@ -11,32 +11,39 @@
                 'patientid' => $_POST['patientid'],
                 'userid' => $_SESSION['id']];
 
+    //Informações relevantes de servidor
     $fonte = $_SERVER['HTTP_REFERER'];
+    $operation = $_POST['operation'];
 
-    //Condensa estes dados em um array para passar para função
-    if (strcmp($operation,'RETRIEVE') == 0){
 
-      //Busca comentários no servidor referente à um paciente
-      $dados = fetchData($assunto['patientid'], $assunto['userid']);
+    //Verifica as operações e as corrige de acordo
+    switch($operation){
 
-      header("Content-type: application/json; charset=UTF-8");
-      print(json_encode($dados,JSON_PRETTY_PRINT));
-    }
-    else if (strcmp($operation,'COMMENT_THIS') == 0){
+      case 'RETRIEVE':
+        //Busca comentários no servidor referente à um paciente
+        $dados = fetchData($assunto['patientid'], $assunto['userid']);
 
+        header("Content-type: application/json; charset=UTF-8");
+        print(json_encode($dados,JSON_PRETTY_PRINT));
+        break;
+
+      case 'COMMENT_THIS':
         if (strcmp($fonte,"labref.php") == 0){
           $assunto['tipo'] = "LAB" ;                   
         }
         else if (strcmp($fonte,"acompanhamento.php") == 0){
           $assunto['tipo'] = "MED";
         }
-
         $comment = new Comment($tipo, $assunto);
+    
+        break;
+
+      case 'EDIT_COMMENT':
+        # code...
+        break;      
     }
-    else if (strcmp($operation,'EDIT_COMMENT') == 0){      
-  	
-    }
-  }
+  }  
+    
   
   //Chegou-se a página via GET. Visualização ocorrerá via GET
   else{
