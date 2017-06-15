92bad32414155540b44c4436b1cb8cee9b98c754
Herperino <l.nasc@live.com>
2017-06-14 21:29:05 -0300

    Testando se o controlador de notas está OK

--- views/notas.php
+++ views/notas.php
@@ -1,15 +1,10 @@
 <!--Chegou-se à pagina via GET-->
 
-<?php if($P_MODE == FALSE):?>
+<?php echo($_SESSION['id'])?>
+
 <div class = 'container'>
     <div class= 'page-header'>
         <h2>Em construção - <?php  echo $mode; ?></h2> 
     </div>
     <img src='https://i.giphy.com/3DnDRfZe2ubQc.gif'/>
 </div>
-
-<!-- Chegou-se à página via POST -->
-<?php else: ?>
-
-
-<?php endif?>
