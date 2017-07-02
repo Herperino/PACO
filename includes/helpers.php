6d4a87eb9e6e143294457fee74dcfa320ff50085
Herperino <l.nasc@live.com>
2017-07-02 15:04:20 -0300

    [DEBUG] Corrigindo fetchData para restaurar comentários

--- includes/helpers.php
+++ includes/helpers.php
@@ -528,7 +528,7 @@ function displayPrescription($prescriptions){
   * ---------------------------------------------*/
   function fetchData($sessionid){
 
-    $id_sessao = "ss_" . $sessionid;
+    $id_sessao = "ss_" . $sessionid . "_".$_SESSION['id'];
     
     $query = pg_query("SELECT * FROM public.\"comments\" WHERE id_sessao LIKE '".$id_sessao."'");
 
