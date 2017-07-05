092fe7398fd0ceb6e7a4cfb3783a5ce2a0409a3e
Herperino <l.nasc@live.com>
2017-07-04 22:48:51 -0300

    [DEV] Primeira tentativa de implementação das visualizações

--- public/js/paco_visuals.js
+++ public/js/paco_visuals.js
@@ -476,8 +476,8 @@ function getCommentNumber(target,paciente){
     else
       comments = data.length;
 
-    console.log(target);
-    
+    console.log(target.id);
+
     target.innerHTML += "<td>"+
                           "<button type='button' style='width:60%;margin-left:15%' onclick='displayComments("+target.id+"," + paciente +")' class='btn btn-default'> Ver Comentários"+
                           "<span class='glyphicon glyphicon-chevron-right' aria-hidden='true'> <span class='badge'>"+comments+"</span></span>"+
