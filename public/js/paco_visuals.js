c17db5d8c72302a0d8a7b336dd2be5f3dbec61c4
Herperino <l.nasc@live.com>
2017-07-06 22:40:46 -0300

    [DEBUG] Button que realiza o displayComments tava errado. Tentando consertar

--- public/js/paco_visuals.js
+++ public/js/paco_visuals.js
@@ -475,9 +475,9 @@ function getCommentNumber(target,paciente){
       comments = data.length;
 
     console.log(target);
-    if (target)
+    if (target != null)
       var call = "onclick=\"displayComments(" + target.id + "," + paciente +")\"";
-      
+
     target.innerHTML += "<td>"+
                           "<button type='button' style='width:60%;margin-left:15%' "+call+" class='btn btn-default'> Ver Comentários"+
                           "<span class='glyphicon glyphicon-chevron-right' aria-hidden='true'> <span class='badge'>"+comments+"</span></span>"+
