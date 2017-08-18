b28908c2349b756948d9470edce3a79aca1cbb35
Herperino <l.nasc@live.com>
2017-08-18 16:07:27 -0300

    [DEBUG] Testando erro do loop

--- public/js/paco_visuals.js
+++ public/js/paco_visuals.js
@@ -99,11 +99,7 @@ function makeCommentList(){
             var content = ""; //Conteúdo a ser inserido no template
 
             console.log(data);
-            if (true){            
-            console.log(data.length);
-            return;
-            }
-
+            
             for(var i = 0; i<data.length;i++){
 
                 pacientes[i] = { name:data[i].patientname,
