95d78d17261f07ceed4a1a46321d07a82f973b49
Herperino <l.nasc@live.com>
2017-08-24 12:30:46 -0300

    [DEBUG]

--- includes/html_components/landing_components.php
+++ includes/html_components/landing_components.php
@@ -3,15 +3,13 @@
     //Define uma templateque vai ser usada na montagem do about_div
     $icon_template = function ($icon_url,$texto){
 
-                $template = "<div class = 'icons col-xs-12 col-sm-4 col-md-4 col-lg-4'>
+                return "<div class = 'icons col-xs-12 col-sm-4 col-md-4 col-lg-4'>
                           <h2>Acompanhe</h2>
                           <img class = 'ico' src='$icon_url'>
                           <br><br>
                           <small>$texto</small>
                         </div>";
-
-
-                return $template;
+;
               };
 
     //Div título do landing
